db49e7f16018404d14a290675660985df7678a68
devJung55 <devJung55@gmail.com>
2023-02-10 17:02:41 +0900

    day05 study

--- workspace/day04/mybanner.js
+++ workspace/day04/mybanner.js
@@ -4,13 +4,16 @@ const banner = document.querySelector("div.banner");
 const imageDiv = document.querySelectorAll("div.banner div");
 const lastImageDiv = document.createElement("div");
 const firstImageDiv = document.createElement("div");
-const next = document.querySelector("div.next");
-const prev = document.querySelector("div.prev");
+const next = document.querySelector(".next");
+const prev = document.querySelector(".prev");
 const buttons = document.querySelectorAll(".buttons button");
 let checkArrow = false;
 let count = 1;
 let auto = setInterval(autoSlide, 2000);
 let temp = buttons[0];
+let pageCount = document.querySelector("#count-length");
+
+
 
 HTMLCollection.prototype.forEach = Array.prototype.forEach;
 buttons.forEach(button => {
@@ -19,40 +22,43 @@ buttons.forEach(button => {
         count = parseInt(button.innerHTML);
         changeButtonStyle();
         banner.style.transition = "transform 0.3s";
-        banner.style.transform = `translate(${-1728 * count}px)`;
+        banner.style.transform = `translate(${-1519 * count}px)`;
         auto = setInterval(autoSlide, 2000);
     });
 });
 
-imageDiv.forEach((div, i) => div.style.backgroundImage = `url(image/00${i+1}.png)`)
+imageDiv.forEach((div, i) => div.style.backgroundImage = `url(image/00${i+1}.jpg)`)
 
 banner.appendChild(lastImageDiv);
-lastImageDiv.style.backgroundImage = `url(image/001.png)`;
+lastImageDiv.style.backgroundImage = `url(image/001.jpg)`;
 
 banner.insertBefore(firstImageDiv, document.querySelector("div.banner div"));
-firstImageDiv.style.backgroundImage = `url(image/00${imageDiv.length}.png)`
+firstImageDiv.style.backgroundImage = `url(image/00${imageDiv.length}.jpg)`
 
-banner.style.transform = `translate(-1728px)`;
+banner.style.transform = `translate(-1519px)`;
 
 function changeButtonStyle() {
     if(temp){
         temp.style.background = "white";
         temp.style.color = "black";
+        
     }
-    buttons[count - 1].style.background = "black";
+    // buttons[count - 1].style.background = "black";
     buttons[count - 1].style.color = "white";
     temp = buttons[count - 1];
+    
+    pageCount.innerHTML =`${count} / ${imageDiv.length}`;
 }
 
 function autoSlide(){
     banner.style.transition = "transform 0.3s";
-    banner.style.transform = `translate(${-1728 * ++count}px)`;
+    banner.style.transform = `translate(${-1519 * ++count}px)`;
     console.log(count);
-    if(count == 7) {
+    if(count == 16) {
         count = 1;
         setTimeout(function(){
             banner.style.transition = "transform 0s";
-            banner.style.transform = "translate(-1728px)";
+            banner.style.transform = "translate(-1519px)";
         }, 300);
     }
     changeButtonStyle();
@@ -63,12 +69,12 @@ prev.addEventListener("click", function(){
     checkArrow = true;
     clearInterval(auto);
     banner.style.transition = "transform 0.3s";
-    banner.style.transform = `translate(${-1728 * --count}px)`;
+    banner.style.transform = `translate(${-1519 * --count}px)`;
     if(count == 0) {
-        count = 6;
+        count = 15;
         setTimeout(function(){
             banner.style.transition = "transform 0s";
-            banner.style.transform = `translate(${-1728 * (imageDiv.length)}px)`;
+            banner.style.transform = `translate(${-1519 * (imageDiv.length)}px)`;
         }, 300);
     }
     changeButtonStyle();
@@ -81,12 +87,12 @@ next.addEventListener("click", function(){
     checkArrow = true;
     clearInterval(auto);
     banner.style.transition = "transform 0.3s";
-    banner.style.transform = `translate(${-1728 * ++count}px)`;
-    if(count == 7) {
+    banner.style.transform = `translate(${-1519 * ++count}px)`;
+    if(count == 16) {
         count = 1;
         setTimeout(function(){
             banner.style.transition = "transform 0s";
-            banner.style.transform = "translate(-1728px)";
+            banner.style.transform = "translate(-1519px)";
         }, 300);
     }
     changeButtonStyle();
